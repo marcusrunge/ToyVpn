4eb3fa63e86ac9bdcc2b8f65a967fcb63060f74b
Marcus Runge <marcus.runge@securepoint.de>
2019-05-09 18:04:59 +0200

    ADDED remaining code

---
+++ ToyVpn/BackgroundTask/VpnBackgroundTask.cs
@@ -0,0 +1,16 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.Networking.Vpn;
+
+namespace BackgroundTask
+{
+    public sealed class VpnBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+            var vpnPlugIn = new ToyVpnPlugin();
+            VpnChannel.ProcessEventAsync(vpnPlugIn, taskInstance.TriggerDetails);
+            deferral.Complete();
+        }
+    }
+}
